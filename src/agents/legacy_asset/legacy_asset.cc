bb3ad6e06470e7c1325869cc97f4864bf70b97aa
Michal Vyskocil <michalvyskocil@eaton.com>
2017-01-03 12:34:38 +0100

    Problem: legacy asset server was pushed out of fty-agent
    Solution: move it to core
    
    Signed-off-by: Michal Vyskocil <MichalVyskocil@eaton.com>

---
+++ src/agents/legacy_asset/legacy_asset.cc
@@ -0,0 +1,48 @@
+/*  =========================================================================
+    legacy_asset_server - Server translating legacy configure messages to new protocol
+
+    Copyright (C) 2017 Eaton
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    =========================================================================
+*/
+
+/*
+@header
+    bios_legacy_asset_server - Server translating legacy configure messages to new protocol
+@discuss
+@end
+*/
+#include "legacy_asset_server.h"
+
+int main () {
+
+    zactor_t *server = zactor_new (legacy_asset_server, (void*) "legacy-asset-server");
+
+    while (true) {
+        char *str = zstr_recv (server);
+        if (str) {
+            puts (str);
+            zstr_free (&str);
+        }
+        else {
+            zsys_info ("Interrupted");
+            break;
+        }
+    }
+
+    zactor_destroy (&server);
+
+}
