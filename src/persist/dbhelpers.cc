f714703e5a6bb31ad847fd1d01c9cdbb5b6ea329
Michal Vyskocil <michalvyskocil@eaton.com>
2017-03-14 12:26:08 +0100

    Problem: is_ok_name has completelly different meaning
    Revert "Problem: is_ok name does not know about utf-8"
    
    This reverts commit 6414d5ae9a85440bd20b6377f3c74495bc7bd8a6.

--- src/persist/dbhelpers.cc
+++ src/persist/dbhelpers.cc
@@ -174,56 +174,30 @@ bool is_ok_element_type (a_elmnt_tp_id_t element_type_id)
     }
 }
 
-// Copyright (c) 2008-2009 Bjoern Hoehrmann <bjoern@hoehrmann.de>
-// See http://bjoern.hoehrmann.de/utf-8/decoder/dfa/ for details.
-
-#define UTF8_ACCEPT 0
-#define UTF8_REJECT 1
-
-static const uint8_t utf8d[] = {
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 00..1f
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 20..3f
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 40..5f
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 60..7f
-  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9, // 80..9f
-  7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7, // a0..bf
-  8,8,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, // c0..df
-  0xa,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x4,0x3,0x3, // e0..ef
-  0xb,0x6,0x6,0x6,0x5,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8, // f0..ff
-  0x0,0x1,0x2,0x3,0x5,0x8,0x7,0x1,0x1,0x1,0x4,0x6,0x1,0x1,0x1,0x1, // s0..s0
-  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,1, // s1..s2
-  1,2,1,1,1,1,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1, // s3..s4
-  1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,3,1,1,1,1,1,1, // s5..s6
-  1,3,1,1,1,1,1,3,1,3,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1, // s7..s8
-};
-
-static uint32_t
-validate_utf8(uint32_t *state, char *str, size_t len) {
-   size_t i;
-   uint32_t type;
-
-    for (i = 0; i < len; i++) {
-        // We don't care about the codepoint, so this is
-        // a simplified version of the decode function.
-        type = utf8d[(uint8_t)str[i]];
-        *state = utf8d[256 + (*state) * 16 + type];
-
-        if (*state == UTF8_REJECT)
-            break;
-    }
-
-    return *state;
-}
-
 bool is_ok_name (const char* name)
 {
     size_t length = strlen (name);
     if ( length == 0)
         return false;
 
-    uint32_t state = UTF8_ACCEPT;
-    validate_utf8 (&state, (char*) name, length);
-    return state == UTF8_ACCEPT;
+    bool is_number = true;
+    for (size_t i = 0; i != length; i++) {
+        if (!isdigit (name [i])) {
+            is_number = false;
+            break;
+        }
+    }
+
+    if (is_number)
+        return false;
+
+    // Bad characters _ % @
+    if (strchr (name, '_') != NULL ||
+        strchr (name, '%') != NULL ||
+        strchr (name, '@') != NULL)
+       return false; 
+
+    return true;
 }
 
 bool is_ok_keytag (const char *keytag)
