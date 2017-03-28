4f3fa32bffece17a58bbf312cc1ef41b60936e38
Jim Klimov <jim@jimklimov.com>
2017-03-28 15:46:45 +0200

    Problem: formatting strings missing in some log_...() calls
    Solution: add "%s" to print single strings

--- src/web/src/asset_computed_impl.cc
+++ src/web/src/asset_computed_impl.cc
@@ -161,7 +161,7 @@ rack_outlets_available(
                 conn, elementId, cb);
 
     } catch (std::exception &e) {
-        log_error(e.what());
+        log_error("%s", e.what());
         return;
     }
 
